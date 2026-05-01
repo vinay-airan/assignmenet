26282c27863df5e7372e78fb409d71cb0c9880a5
vinay-airan <vinayairan11@gmail.com>
2026-05-01 14:17:35 +0530

    fix: update BASE_URL assignment to prioritize environment variable

--- frontend/src/api/forgeClient.js
+++ frontend/src/api/forgeClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
-const BASE_URL = "http://localhost:5000/api";
+ const BASE_URL = import.meta.env.VITE_API_URL ;
+//const BASE_URL = "http://localhost:5000/api";
 const forgeClient = axios.create({
   baseURL: BASE_URL,
   headers: { "Content-Type": "application/json" },
